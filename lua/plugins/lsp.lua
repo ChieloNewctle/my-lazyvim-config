92bae3beb99fbecae9b0e108f5eae3065aa7a19b
Chielo Newctle <ChieloNewctle@Yandex.com>
2023-08-02 12:37:31 +0800

    update lsp settings

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      autoformat = false,
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+        sources = {
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.shfmt,
+          nls.builtins.formatting.black,
+        },
+      }
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+      },
+    },
+  },
+}
