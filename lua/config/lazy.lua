a26cd674a94478c3be311d01c4705fb9cbd675dc
Chielo Newctle <ChieloNewctle@Yandex.com>
2023-09-01 09:54:43 +0800

    update settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- lang
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opt = {
+      indent = {
+        enable = false,
+      },
+    },
+  },
+  { "nvim-treesitter/nvim-treesitter-context" },
+}
