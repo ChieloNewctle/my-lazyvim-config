96d74f78d5abcf897059ca1d24789d3a46cf1600
Chielo Newctle <ChieloNewctle@Yandex.com>
2023-10-18 13:56:36 +0800

    update formatters

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,4 +1,4 @@
-local function prepend_formatter_args(old_args, extra_args)
+local function prepend_formatter_args(condition, old_args, extra_args)
   if type(old_args) == "string" then
     old_args = { old_args }
   end
@@ -6,41 +6,60 @@ local function prepend_formatter_args(old_args, extra_args)
     extra_args = { extra_args }
   end
   local function args(ctx)
-    if type(old_args) == "table" then
-      return { unpack(extra_args), unpack(old_args) }
+    if condition() then
+      if type(old_args) == "table" then
+        return { unpack(extra_args), unpack(old_args) }
+      end
+      return { unpack(extra_args), unpack(old_args(ctx)) }
     end
-    return { unpack(extra_args), unpack(old_args(ctx)) }
+    if type(old_args) == "table" then
+      return { unpack(old_args) }
+    end
+    return { unpack(old_args(ctx)) }
   end
   return args
 end
 
-require("conform.formatters.black").args = prepend_formatter_args(require("conform.formatters.black").args, "-S")
-require("conform.formatters.darker").args = prepend_formatter_args(require("conform.formatters.darker").args, "-S")
+local default_python_formatter_opt = {
+  use_darker_for_python = false,
+  skip_string_literal = true,
+}
 
-require("neoconf.plugins").register({
-  name = "use_darker_for_python",
-  on_schema = function(schema)
-    schema:import("use_darker_for_python", { enabled = false })
-  end,
-})
-
-require("conform").formatters_by_ft.c = { "clang_format" }
-require("conform").formatters_by_ft.cpp = { "clang_format" }
-require("conform").formatters_by_ft.css = { "prettier" }
-require("conform").formatters_by_ft.html = { "prettier" }
-require("conform").formatters_by_ft.javascript = { "prettier" }
-require("conform").formatters_by_ft.json = { "fixjson", "prettier" }
-require("conform").formatters_by_ft.lua = { "stylua" }
-require("conform").formatters_by_ft.markdown = { "prettier" }
-require("conform").formatters_by_ft.python =
-  { require("neoconf").get("use_darker_for_python", { enabled = false }).enabled and "black" or "darker" }
-require("conform").formatters_by_ft.sh = { "shfmt" }
-require("conform").formatters_by_ft.typescript = { "prettier" }
-require("conform").formatters_by_ft.typescriptreact = { "prettier" }
-require("conform").formatters_by_ft.toml = { "taplo" }
-require("conform").formatters_by_ft.yaml = { "prettier" }
+require("conform.formatters.black").args = prepend_formatter_args(function()
+  return require("neoconf").get("python_formatter", default_python_formatter_opt).skip_string_literal
+end, require("conform.formatters.black").args, "-S")
+require("conform.formatters.darker").args = prepend_formatter_args(function()
+  return require("neoconf").get("python_formatter", default_python_formatter_opt).skip_string_literal
+end, require("conform.formatters.darker").args, "-S")
 
 return {
+  {
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim", "folke/neoconf.nvim" },
+    opts = function()
+      return {
+        formatters_by_ft = {
+          c = { "clang_format" },
+          cpp = { "clang_format" },
+          css = { "prettier" },
+          html = { "prettier" },
+          javascript = { "prettier" },
+          json = { "fixjson", "prettier" },
+          lua = { "stylua" },
+          markdown = { "prettier" },
+          python = {
+            require("neoconf").get("python_formatter", default_python_formatter_opt).use_darker_for_py and "darker"
+              or "black",
+          },
+          sh = { "shfmt" },
+          typescript = { "prettier" },
+          typescriptreact = { "prettier" },
+          toml = { "taplo" },
+          yaml = { "prettier" },
+        },
+      }
+    end,
+  },
   {
     "williamboman/mason.nvim",
     opts = {
