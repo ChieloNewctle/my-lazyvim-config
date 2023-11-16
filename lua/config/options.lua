269b094e00f93b3f3ac9d87bc508c3a4228e809a
Chielo Newctle <ChieloNewctle@Yandex.com>
2023-11-16 15:31:45 +0800

    add padding to neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -34,10 +34,10 @@ local function set_neovide_options()
   vim.o.linespace = 0
   vim.g.neovide_scale_factor = 1.0
 
-  vim.g.neovide_padding_top = 0
-  vim.g.neovide_padding_bottom = 0
-  vim.g.neovide_padding_right = 0
-  vim.g.neovide_padding_left = 0
+  vim.g.neovide_padding_top = 4
+  vim.g.neovide_padding_bottom = 4
+  vim.g.neovide_padding_right = 4
+  vim.g.neovide_padding_left = 4
 
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
