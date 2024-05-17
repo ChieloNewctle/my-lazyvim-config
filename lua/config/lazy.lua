6436aecb67560e67376d3e65967aca253d259937
Chielo Newctle <ChieloNewctle@Yandex.com>
2024-05-17 09:11:29 +0800

    remove mini animate

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,7 +32,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.util.dot" },
