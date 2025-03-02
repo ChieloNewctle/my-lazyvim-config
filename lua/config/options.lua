b4503be31bcb4a9c55b7badca675c916c6ae7044
Chielo Newctle <ChieloNewctle@gmail.com>
2025-03-02 22:34:33 +0800

    fix: after enable wslg and install xclip, fix is not required for wsl

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,28 +2,26 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.o.clipboard = ""
-
--- WSL Clipboard support
-local is_wsl = vim.fn.has("wsl") == 1
-if is_wsl then
-  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
-  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
-  vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
-    },
-    paste = {
-      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
-else
-  vim.cmd("set clipboard+=unnamedplus")
-end
+-- -- WSL Clipboard support
+-- local is_wsl = vim.fn.has("wsl") == 1
+-- if is_wsl then
+--   -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+--   -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+--   vim.g.clipboard = {
+--     name = "WslClipboard",
+--     copy = {
+--       ["+"] = "powershell.exe -noprofile -c \"chcp 65001 >$null; clip.exe\"",
+--       ["*"] = "powershell.exe -noprofile -c \"chcp 65001 >$null; clip.exe\"",
+--     },
+--     paste = {
+--       ["+"] = 'powershell.exe -noprofile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--       ["*"] = 'powershell.exe -noprofile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     },
+--     cache_enabled = 0,
+--   }
+-- else
+--   vim.cmd("set clipboard+=unnamedplus")
+-- end
 
 vim.g.lazyvim_picker = "telescope"
 
